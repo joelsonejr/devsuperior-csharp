a8326fbd93c3695a35064c1df72903432132ed64
Joelson Junior <joelson.ejr@gmail.com>
2024-06-03 21:03:33 -0300

    Concluído o desfio.

--- DevSuperior/DesafioImposto.cs
+++ DevSuperior/DesafioImposto.cs
@@ -5,6 +5,8 @@ using System.Globalization;
 namespace Course {
     class DesafioImposto {
         static void Main(string[] args) {
+
+            Console.WriteLine();
         
             Console.Write("Digite sua renda anual com salário: ");
             double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -28,74 +30,84 @@ namespace Course {
             double maximoDedutivel = 0;
             double gastosDedutiveis;
             double abatimento;
+            double taxaSalario;
 
             if (salario/12 < 3000)
             {
-                impostoSobreSalario = 0;
+                taxaSalario = 0;
             }
             else if (salario/12 < 5000)
             {
-                impostoSobreSalario = salario * 0.1;
+                taxaSalario =  0.1;
             }
             else 
             {
-                impostoSobreSalario = salario * 0.2;
+                taxaSalario = 0.2;
             }
+
+            impostoSobreSalario = calculaImposto(salario, taxaSalario);
 
 
             if (ganhoPrestacaoServicos > 0 )
             {
-                impostoServicos = ganhoPrestacaoServicos * 0.15;
+                impostoServicos = calculaImposto(ganhoPrestacaoServicos, 0.15);
             }
 
 
             if (ganhoCapital > 0)
             {
-                impostoGanhoCapital = ganhoCapital * 0.2;
+                impostoGanhoCapital = calculaImposto(ganhoCapital, 0.2);
             }
 
 
             impostoBrutoTotal = impostoSobreSalario + impostoServicos + impostoGanhoCapital;
 
-
             gastosDedutiveis = gastosEducacionais + gastosMedicos;
 
-            if (gastosDedutiveis > 0) 
-            {
+            maximoDedutivel = calculaImposto(impostoBrutoTotal, 0.3);
 
-                if (gastosDedutiveis > impostoBrutoTotal * 0.3) 
-                {
-                    maximoDedutivel = impostoBrutoTotal * 0.3;
-                }
-                else
-                {
-                    maximoDedutivel = gastosDedutiveis;
-                }
+
+            if (gastosDedutiveis >= maximoDedutivel) 
+            {
+                abatimento = maximoDedutivel;
+            }
+            else 
+            {
+                abatimento = gastosDedutiveis;
             }
 
-            abatimento = maximoDedutivel;
+
             impostoDevido = impostoBrutoTotal - abatimento;
 
+            imprimirRelatorio(impostoSobreSalario, impostoServicos, impostoGanhoCapital, maximoDedutivel, gastosDedutiveis, impostoBrutoTotal, abatimento, impostoDevido);
 
-            
+        }
+
+        static double calculaImposto(double valor, double taxa) 
+            {
+                return valor * taxa;
+            }
+
+        static void imprimirRelatorio(double impSalario, double impServicos, 
+            double impCapital, double maxDedutivel, double gastosDedutiveis, 
+            double impTotal, double abatimento, double impDevido)   
+        {
             Console.WriteLine();
             Console.WriteLine("RELATÓRIO DE IMPOSTO DE RENDA");
             Console.WriteLine();
             Console.WriteLine("CONSOLIDADO DE RENDA:");
-            Console.WriteLine($"Imposto sobre salário: {impostoSobreSalario:F2}");
-            Console.WriteLine($"Imposto sobre serviços: {impostoServicos:F2}");
-            Console.WriteLine($"Imposto sobre ganho de capital: {impostoGanhoCapital:F2}");
+            Console.WriteLine($"Imposto sobre salário: {impSalario:F2}");
+            Console.WriteLine($"Imposto sobre serviços: {impServicos:F2}");
+            Console.WriteLine($"Imposto sobre ganho de capital: {impCapital:F2}");
             Console.WriteLine();
             Console.WriteLine("DEDUÇÕES:");
-            Console.WriteLine($"Máximo dedutível: {maximoDedutivel:F2}");
+            Console.WriteLine($"Máximo dedutível: {maxDedutivel:F2}");
             Console.WriteLine($"Gastos dedutíveis: {gastosDedutiveis:F2}");
             Console.WriteLine();
             Console.WriteLine("RESUMO:");
-            Console.WriteLine($"Imposto bruto total: {impostoBrutoTotal:F2}");
+            Console.WriteLine($"Imposto bruto total: {impTotal:F2}");
             Console.WriteLine($"Abatimento: {abatimento:F2}");
-            Console.WriteLine($"Imposto devido: {impostoDevido:F2}");
-
-
+            Console.WriteLine($"Imposto devido: {impDevido:F2}");
         }
 
     }
