453e49964900230fa8ebd777f1ccf504a20ecc60
Joelson Junior <joelson.ejr@gmail.com>
2024-06-08 13:13:40 -0300

    Concluídos exercícios de fixação

--- DevSuperior/Exercicios-3.cs
+++ DevSuperior/Exercicios-3.cs
@@ -1,4 +1,35 @@
+/* using System.Globalization;
+
 namespace Course
+{
+    class Exercicio303
+    {
+        public static void Main(string[] args)
+        {   
+            Aluno aluno = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as 3 notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"NOTA FINAL: {aluno.CalculaNotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine(aluno.StatusFinal());
+
+            if (aluno.Status == "REPROVADO")
+            {
+                Console.WriteLine($"FALTARAM {aluno.CalcularNotaFaltante():F2} PONTOS");
+            }
+
+        }
+    }
+} */
+
+
+/* namespace Course
 {
     class Exercicio302
     {
@@ -27,16 +58,7 @@ namespace Course
             Console.WriteLine(funcionario);
         }
     }
-}
-
-
-
-
-
-
-
-
-
+} */
 
 
 /* using System;
