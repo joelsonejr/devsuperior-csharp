76e52ac69da31ada1be94ba82b66fbb8fa2f9477
Joelson Junior <joelson.ejr@gmail.com>
2024-10-22 12:51:33 -0300

    Solved problem, without Interface

--- DevSuperior/Program.cs
+++ DevSuperior/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Globalization;
 using Course.Entities;
+using Course.Services;
 
 namespace Course
 {
@@ -16,8 +17,19 @@ namespace Course
             DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
             Console.Write("Return (dd/MM/yyyy hh:ss): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
 
         }   
     }
