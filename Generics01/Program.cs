b3ba8af5f1df0f3c143c54ed75d33c3a6858e4d8
Joelson Junior <joelsonjr31@gmail.com>
2025-04-08 17:27:09 -0300

    Aula 01 de Generics

--- Generics01/Program.cs
+++ Generics01/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 
-namespace Generics {
+namespace LeGenerics {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello MoFo");
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for( int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+
+            Console.WriteLine("First: " + printService.First());
         }
     }
 }
